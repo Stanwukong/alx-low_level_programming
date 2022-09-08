f6772ab9bde85142a7565d035eb30d99fe20372a
Stanwukong <stanleyajanaku@gmail.com>
2022-09-08 15:54:00 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,6 @@
 
 /**
  * main - print the string in the put function
- *
  * Description: using the main function
  * this program prints "with proper grammar, but the outcome is a piece of art,"
  * return: 0
