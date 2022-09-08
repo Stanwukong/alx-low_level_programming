b4f80936944a2c2b7e8180e3cc875bb3cdc216a3
Stanwukong <stanleyajanaku@gmail.com>
2022-09-08 16:37:01 -0700

    task 5 updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - print the string in the put function
- * Description: using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art,"
- * return: 0
+ * main - main block
+ * Return: 0
  **/
 int main(void)
 {
