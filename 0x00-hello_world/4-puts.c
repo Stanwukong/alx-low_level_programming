36d2965d5d8453a6beba93bda9ca3ef80593f49a
Stanwukong <stanleyajanaku@gmail.com>
2022-09-08 15:33:09 -0700

    4 updated*

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - Entry point
  * 
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
