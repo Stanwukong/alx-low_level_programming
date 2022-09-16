4a0686f3b5a894678d9b30caf25eaa6e3e2e9d51
Stanwukong <stanleyajanaku@gmail.com>
2022-09-15 17:10:27 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /*
- * while (i < 10)
- * {
- * putchar(i);
- * }
- */
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
 printf("Infinite loop avoided! \\0/\n");
 
 return (0);
