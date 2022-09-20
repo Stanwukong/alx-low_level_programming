4d1022885362534053fde89a45465c3b040cb540
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-09-20 22:16:53 +0100

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * swap_int - given two integers swap the values they are holding 
+ * swap_int - given two integers swap the values they are holding
  * @a: parameter 1
  * @b: parameter 2
  */
- void swap_int(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 int tmp;
 
