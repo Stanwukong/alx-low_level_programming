b272c22f6cfabc41da24a4fbbb48baf8864d25a5
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-09-20 22:26:54 +0100

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function to get the length of a string
+ * @s: string pointer to passed to yhis function
+ * Return: returns length of the string
+ */
+int _strlen(char *s)
+{
+int len;
+
+while(*s != '\0')
+{
+len += 1;
+*s = *s + 1;
+}
+return (len);
+}
