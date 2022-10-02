8b6f319d3debe433478bd5870ba38f85e3d472ad
root <root@Stanwukong.localdomain>
2022-10-02 09:46:39 +0100

    Random commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 /**
- *main - prints the standard error sandwiched around the standard output 
+ *main - prints the standard error sandwiched around the standard output
  *Return: returns 1
  **/
 int main(void)
