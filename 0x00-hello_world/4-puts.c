7084b96adf76bab344957749905be1eca167f12c
Stanwukong <stanleyajanaku@gmail.com>
2023-07-13 10:43:45 -0700

    Fixed betty error: try 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 /**
- * main: Prints "Programming is like building a multilingual 
+ * main - Prints "Programming is like building a multilingual
  *		puzzle, followed by a new line.
  *
  * Return: Always 0.
