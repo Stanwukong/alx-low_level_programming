d3703f57b24a187d50e226ba653be37c6585c446
Stanwukong <stanleyajanaku@gmail.com>
2023-07-18 16:34:00 -0700

    added header file

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,20 @@
+/*
+ * File: _putchar.c
+ *
+ * Author: Stanley O. Ajanaku
+ */
+
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
