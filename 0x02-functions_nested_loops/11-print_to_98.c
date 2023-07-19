1c7a0a24f24599d0076aa7d2c2d1eda18870aa47
Stanwukong <stanleyajanaku@gmail.com>
2023-07-18 18:19:32 -0700

    Fixed checker error: try 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,12 +18,10 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		do
+		while (n > 98)
 		{
-		printf("%d, ", n--);
+			printf("%d, ", n--);
 		}
-		while (n > 98);
-
 		printf("%d\n", n);
 	}
 	else
