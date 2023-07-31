a4fc2db82ec1a851a7191f52ba0742ba76513380
Stanwukong <stanleyajanaku@gmail.com>
2023-07-31 16:33:12 -0700

    fixed betty error

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -24,15 +24,13 @@ char *_strstr(char *haystack, char *needle)
 
 		if (haystack[index] == needle[index])
 		{
-			do
-			{
+			do {
 				if (needle[index + 1] == '\0')
 				{
 					return (haystack);
 				}
 				index++;
-			}
-			while (haystack[index] == needle[index]);
+			} while (haystack[index] == needle[index]);
 		}
 
 		haystack++;
