4814b68465fefcdd344e1941b02d0084ff831aa5
Stanwukong <stanleyajanaku@gmail.com>
2023-08-15 13:15:46 -0700

    Last call...

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdlib.h>
-
 /**
  * struct dog - New type defining a dog.
  * @name: Name of the dog.
@@ -16,5 +14,15 @@ struct dog
 	float age;
 	char *owner;
 }
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
