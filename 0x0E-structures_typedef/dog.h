00e7360ed661667b14d9b04958be47fa8bcf1844
Stanwukong <stanleyajanaku@gmail.com>
2023-08-15 13:00:21 -0700

    New Project Babygit add ..!

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - New type defining a dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+}
+
+
