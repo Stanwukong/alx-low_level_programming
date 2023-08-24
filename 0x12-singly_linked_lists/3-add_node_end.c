6bc3731989f6396fe3ecefd2f7d58e2adc9d67ef
Stanwukong <stanleyajanaku@gmail.com>
2023-08-24 14:14:03 -0700

    Task 3: Check two

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@
  *	On fail -> NULL
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	int len;
 	char *newStr;
@@ -19,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t lastNode;
 
 	newNode = malloc(sizeof(list_t));
-	if (newNode == 	NULL)
+	if (newNode ==	NULL)
 	{
 		return (NULL);
 	}
