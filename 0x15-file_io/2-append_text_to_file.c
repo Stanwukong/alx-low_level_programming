8c8bb805f4cf4e8190ff4b5a2e4b835824a6ed8d
Stanwukong <stanleyajanaku@gmail.com>
2023-09-08 07:19:26 -0700

    Check for NULL content

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,6 +17,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
 	/* Find length of text_content */
 	while (text_content[idx] != '\0')
 	{
