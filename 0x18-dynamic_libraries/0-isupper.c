065d57a9d8f403fb61ff6e2ff9691880a608ae27
Stanwukong <stanleyajanaku@gmail.com>
2023-10-17 09:15:11 -0700

    Setup files

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-isupper.c
+ *
+ * Author: Stanley O. Ajanaku
+ */
+
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: Character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
