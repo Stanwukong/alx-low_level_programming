b48abdb2905baf29323fbf60d1198e51d4db65aa
Stanwukong <stanleyajanaku@gmail.com>
2023-10-17 10:40:31 -0700

    Task 2: Dynamic lib that calls C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int add(int a , int b)
+{
+	int result = a + b;
+	return (result);
+}
+
+
+int sub(int a, int b)
+{
+	int result = a - b;
+	return (result);
+}
+
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
+
+int div(int a, int b)
+{
+	int result = a / b;
+	return (result);
+}
+
+int mod(int a, int b)
+{
+	int result = a % b;
+	return (result);
+}
