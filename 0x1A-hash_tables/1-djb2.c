28512aa226da5ab5bf52e0d9c92894a14b2433bb
Stanwukong <stanleyajanaku@gmail.com>
2023-11-16 01:43:40 -0800

    Task 1: Check 3

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,3 @@
-#include "hash_tables.h"
-
 /**
  * hash_djb2 - Hash function implementing the
  *               djb2 algorithm.
@@ -7,13 +5,15 @@
  *
  * Return: The hash value.
  */
-unsigned long int hash_djb2(const char *str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash = 5381;
+	unsigned long int hash;
 	int c;
 
+	hash = 5381;
 	while ((c = *str++))
-		hash = ((hash << 5) + hash) + c;
-
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	return (hash);
 }
