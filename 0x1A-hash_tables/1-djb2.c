555d28fba60e4486433aa264de7cc2073329d1cd
Stanwukong <stanleyajanaku@gmail.com>
2023-11-16 01:26:37 -0800

    Task 1: Implementing djb2 algorithm

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implementing the
+ *               djb2 algorithm.
+ * @str: The string to be hashed.
+ *
+ * Return: The hash value.
+ */
+unsigned long int  hash_djb2(const char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+
+	return (hash);
+}
