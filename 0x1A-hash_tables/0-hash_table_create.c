86bf0f8eaaca7fc7d98457a4b5b3ada614860106
Stanwukong <stanleyajanaku@gmail.com>
2023-11-16 01:06:49 -0800

    Task 0: Check 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -30,7 +30,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	for (i = 0; i > size; i++)
 	{
-		new_table->array[i] = NULL;
+		(new_table->array)[i] = NULL;
 	}
 
 	return (new_table);
