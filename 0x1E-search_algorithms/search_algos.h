5ee26e8bd55d3e46da9378d2ea8ddcabe76013be
Stanwukong <stanleyajanaku@gmail.com>
2024-03-13 10:47:57 -0700

    Advanced tasks(2)

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,22 @@
 #include <math.h>
 
 /* Data Structures */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
