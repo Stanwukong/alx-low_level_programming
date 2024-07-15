e8712cea270ddd799d418454ebc20b169dee4dd7
Oluwafemi S.E. Ajanaku <111810384+Stanwukong@users.noreply.github.com>
2024-07-15 14:11:11 +0100

    Tasks completed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a vlaue in a sorted array of
+ * binary_search - Searches for a value in a sorted array of
  *		using the Binary Search algorithm.
  * @array: A pointer to the first element of the array to search.
  * @size: Number of elements in the array.
